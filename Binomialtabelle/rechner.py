61018d9469deb80bd77c0dfc1f2544762147e4a8
WavyCat <maxkurtenbach@outlook.de>
2023-04-25 16:36:59 +0200

    Einfach alles(Fast)

--- Binomialtabelle/rechner.py
+++ Binomialtabelle/rechner.py
@@ -31,17 +31,73 @@ def rechnen():
     #Alle drei Checkboxen(kumuliert, invertiert, und kumuliert Differenz) werden in Variablen gespeichert
     inv = js.document.querySelector("#v").checked
     kum = js.document.querySelector("#k").checked
-    kumS = js.document.querySelector("#kS").checked
+    kumS = js.document.querySelector("#kS").checked 
+    
+    #Checksummen für kum und kumS
+    x1=0
+    x2=0
 
-    #Es kann entweder k oder kS gedrückt werden nicht beides
-    if kum:
-        js.document.querySelector("#kS").setChecked(unchecked)
-    if kumS:
-        js.document.querySelector("#k").setChecked(unchecked)   
-    x=0
     #Ausgabefeld wird gelöscht
     Element("ergebnis").element.innerHTML = ""
 
     #Eingabe Überprüfung
-    
+    try:
+        n = int(Element("n").value)
+    except:
+        Element("ergebnis").element.innerHTML += "Bitte für n eine natürliche Zahl eingeben.<br />"
+    else:
+        x1+=1
+        x2+=1
 
+    try:
+        p = float(Element("p").value)
+    except:
+        Element("ergebnis").element.innerHTML += "Bitte für p eine reele Zahl zwischen 0 und 1 eingeben.<br />"
+    else:
+        x1+=1
+        x2+=1
+
+    if kum:
+        try:
+            r = int(Element("r").value)
+        except:
+            Element("ergebnis").element.innerHTML += "Bitte für r eine natürliche Zahl eingeben.<br />"
+        if n>=r:
+            x1+=1
+        else:
+           Element("ergebnis").element.innerHTML += "Bitte für n eine natürliche Zahl kleiner gleich n eingeben.<br />" 
+    
+    if kumS:
+        try:
+            r1 = int(Element("r1").value)
+        except:
+            Element("ergebnis").element.innerHTML += "Bitte für r1 eine natürliche Zahl eingeben.<br />"
+        else:
+            x2+=1
+
+        try:
+            r2 = int(Element("r2").value)
+        except:
+            Element("ergebnis").element.innerHTML += "Bitte für r2 eine natürliche Zahl eingeben.<br />"
+        else:
+            x2+=1
+        
+        if r1>r2:
+            Element("ergebnis").element.innerHTML += "Bitte für r2 eine natürliche Zahl größer r1 eingeben.<br />"
+        else:
+            x2+=1
+        if (r1>n) or (r2>n):
+            Element("ergebnis").element.innerHTML += "Bitte für r2 und r1 eine natürliche Zahl kleiner n eingeben.<br />"
+        else:
+            x2+=1
+    
+    if x1 == 3:
+        if inv:
+            Element("ergebnis").element.innerHTML += "1-P("+str(r)+" ≤ X)="+str(1-f(n,p,r))
+        else:
+            Element("ergebnis").element.innerHTML += "P("+str(r)+" ≤ X)="+str(f(n,p,r))
+    if x2 == 6:
+        if inv:
+            Element("ergebnis").element.innerHTML += "1-P("+str(r1)+" ≤ X ≤ "+str(r2)+")="+str(1-fdiff(n,p,r1,r2))
+        else:
+            Element("ergebnis").element.innerHTML += "P("+str(r1)+" ≤ X ≤ "+str(r2)+")="+str(fdiff(n,p,r1,r2))
