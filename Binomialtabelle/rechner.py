ba3681db04734af219712a79b8657387ee58ed91
WavyCat <maxkurtenbach@outlook.de>
2023-04-25 16:11:16 +0200

    lol

--- Binomialtabelle/rechner.py
+++ Binomialtabelle/rechner.py
@@ -28,50 +28,21 @@ def sigmaP(sig,n,p):
     return fdiff(n,p,math.ceil(mu-sig*sigma),math.floor(mu+sig*sigma))
 
 def rechnen():
+    #Alle drei Checkboxen(kumuliert, invertiert, und kumuliert Differenz) werden in Variablen gespeichert
+    inv = js.document.querySelector("#v").checked
+    kum = js.document.querySelector("#k").checked
+    kumS = js.document.querySelector("#kS").checked
+
+    #Es kann entweder k oder kS gedrückt werden nicht beides
+    if kum:
+        js.document.querySelector("#kS").setChecked(unchecked)
+    if kumS:
+        js.document.querySelector("#k").setChecked(unchecked)   
     x=0
+    #Ausgabefeld wird gelöscht
     Element("ergebnis").element.innerHTML = ""
 
-    try:
-        n = int(Element("n").value)
-    except:
-        Element("ergebnis").element.innerHTML = "Bitte für n eine natürliche Zahl eingeben.<br />"
-    else:
-        x+=1
+    #Eingabe Überprüfung
+    if kum:
 
-    try:
-        p = float(Element("p").value)
-    except:
-        Element("ergebnis").element.innerHTML += "Bitte für p eine Zahl x eingeben für die gilt 0≤x≤1.<br />"
-    else:
-        x+=1
 
-    try:
-        r = int(Element("r").value)
-    except:
-        Element("ergebnis").element.innerHTML += "Bitte für r eine natürliche Zahl eingeben.<br />"
-    else:
-        if x==2:
-            if n>=r:
-                x+=1
-            else:
-                Element("ergebnis").element.innerHTML += "Bitte für r eine natürliche Zahl kleiner gleich n eingeben.<br />"
-
-    try:
-        r1 = int(Element("r1").value)
-    except:
-        Element("ergebnis").element.innerHTML += "Bitte für r1 eine natürliche Zahl eingeben.<br />"
-
-    if x == 3:
-        k = js.document.querySelector("#k").checked
-        v = js.document.querySelector("#v").checked
-
-        if k:
-            if v:
-                Element("ergebnis").write("1 - P(X ≤ " + str(r) + ") = " + str((1 - f(n, p, r)) * 100) + "%")
-            else:
-                Element("ergebnis").write("P(X ≤ " + str(r) + ") = " + str(f(n, p, r) * 100) + "%")
-        if not k:
-            if v:
-                Element("ergebnis").write("1 - P(X = " + str(r) + ") = " + str((1 - b(n, p, r)) * 100) + "%")
-            else:
-                Element("ergebnis").write("P(X = " + str(r) + ") = " + str(b(n, p, r) * 100) + "%")
