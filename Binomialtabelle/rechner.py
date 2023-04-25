f8aaf1fe193b2d3ff0f2938391f03ddddd18f25e
WavyCat <maxkurtenbach@outlook.de>
2023-04-25 19:18:30 +0200

    meme

--- Binomialtabelle/rechner.py
+++ Binomialtabelle/rechner.py
@@ -27,8 +27,11 @@ def sigmaP(sig,n,p):
     mu, sigma = musig(n,p)
     return fdiff(n,p,math.ceil(mu-sig*sigma),math.floor(mu+sig*sigma))
 
-def hidclick():
-    print("Hello")
+def hidclick1():
+    Element("containsr2").add_class("hidden")
+
+def hidclick3():
+    Element("containsr2").remove_class("hidden")
 
 def rechnen():
     #Alle drei Checkboxen(kumuliert, invertiert, und kumuliert Differenz, SigmaInterval) werden in Variablen gespeichert
