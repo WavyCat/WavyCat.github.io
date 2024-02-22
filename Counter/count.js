490b33a3c9c3794ef85e765a6fa8567116e074a1
WavyCat <96064769+WavyCat@users.noreply.github.com>
2024-02-22 14:49:28 +0100

    Add files via upload

---
+++ Counter/count.js
@@ -0,0 +1,15 @@
+document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 171) { //Plus
+        document.getElementById("team1").innerHTML = (parseInt(document.getElementById("team1").innerHTML) + 1);
+
+    }
+    else if (event.keyCode == 173) { //Minus
+        document.getElementById("team1").innerHTML = (parseInt(document.getElementById("team1").innerHTML) - 1);
+    }
+    else if (event.keyCode == 38) { //Hoch
+        document.getElementById("team2").innerHTML = (parseInt(document.getElementById("team2").innerHTML) + 1);
+    }
+    else if (event.keyCode == 40) { //Runter
+        document.getElementById("team2").innerHTML = (parseInt(document.getElementById("team2").innerHTML) - 1);
+    }
+}, true);
